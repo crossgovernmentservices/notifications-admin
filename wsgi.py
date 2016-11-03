7af44aa1d4d2d17dee97b09cf570672d71726673
Andy Driver <andy@pagezero.net>
2016-11-03 17:00:37 +0000

    Wrap app in AGS middleware

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,15 @@ from credstash import getAllSecrets
 import os
 
 # on aws get secrets and export to env
-os.environ.update(getAllSecrets(region="eu-west-1"))
+#os.environ.update(getAllSecrets(region="eu-west-1"))
 
 from app import create_app  # noqa
 
 application = create_app()
+
+import ags
+
+application.wsgi_app = ags.Client(application.wsgi_app)
 
 if __name__ == "__main__":
         application.run()
