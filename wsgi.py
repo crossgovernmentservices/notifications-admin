4f0fc45897e9f73e2e6e347a1e5046c71e052fd1
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2016-11-22 15:17:20 +0000

    cleaned up code

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,13 @@
 from credstash import getAllSecrets
+import ags
 import os
 
 # on aws get secrets and export to env
-#os.environ.update(getAllSecrets(region="eu-west-1"))
+os.environ.update(getAllSecrets(region="eu-west-1"))
 
 from app import create_app  # noqa
 
 application = create_app()
-
-import ags
 
 application.wsgi_app = ags.Client(application.wsgi_app)
 
