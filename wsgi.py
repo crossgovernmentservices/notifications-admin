636b6c9344306931bbb91be081ccce4c05c33a22
Andy Driver <andy@pagezero.net>
2016-12-12 10:37:24 +0000

    Fixes to integrate with refactored AGS client

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import ags
 import os
 
 # on aws get secrets and export to env
-os.environ.update(getAllSecrets(region="eu-west-1"))
+# os.environ.update(getAllSecrets(region="eu-west-1"))
 
 from app import create_app  # noqa
 
